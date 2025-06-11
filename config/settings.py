873e16cc6553153f89164ae7ecfac76ebfb5fbad
Aleksandr Sitnikov <aleksandrsit68@gmail.com>
2025-06-11 20:30:25 +0300

    Подключить django-environ

--- config/settings.py
+++ config/settings.py
@@ -12,21 +12,30 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import sys
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(BASE_DIR / 'apps'))
 
+# Инициализация
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+# Чтение файла .env
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cds-!z0zqry6ic8nk3m*47vw&^%w)9=o=@yjfmsvarvq3vngvt'
-
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
